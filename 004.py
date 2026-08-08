7455b01034278f05d85d0b82497f38b9d85a4a68
Nesara M S <nesarakushal@gmail.com>
2026-08-08 11:51:38 +0530

    Add files via upload

---
+++ 004.py
@@ -0,0 +1,9 @@
+'''To check if a list contains a palindrome of elemrnt (hint;use copy()method)'''
+list1 = [1,2,1]
+list2 = [1,2,3]
+copy_list1 = list1.copy()
+copy_list1.reverse()
+if copy_list1 == list1:
+    print("list1 is a palindrome")
+else:
+    print("list2 is not a palindrome")
