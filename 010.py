3fc02ba61db2fcb98e946fdf69e2e32f4fed5a07
Nesara M S <nesarakushal@gmail.com>
2026-08-12 19:47:07 +0530

    Add files via upload

---
+++ 010.py
@@ -0,0 +1,7 @@
+'''WAP to print the fibonacci series up to N terms using for loop.'''
+
+n = int(input("Enter the number of terms: "))
+a, b = 0, 1 
+for _ in range(n):
+    print(a)
+    a, b = b, a + b 
