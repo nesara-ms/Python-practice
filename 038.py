3dc5d058dd94396d6be547573da97a93b3edfdf8
Nesara M S <nesarakushal@gmail.com>
2026-09-07 22:15:20 +0530

    Add files via upload

---
+++ 038.py
@@ -0,0 +1,7 @@
+'''using math module to find the power of a number'''
+import math
+
+base = int(input("Enter the base: "))
+exponent = int(input("Enter the exponent: "))
+result = math.pow(base, exponent)
+print("Result:", result)
